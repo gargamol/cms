f56b28dc75965b5173695487aafb5abec222670a
Jeremy Laird <Jeremy.Laird@cygnus.com>
2020-09-17 16:53:41 -0500

    Basic Graph Example

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,42 +5,41 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/apollo',
-  ],
+  modules: ['@nuxtjs/apollo'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  },
+  build: {},
 
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'https://picon.graphql.base-cms.io',
-      }
-    }
-  }
-}
+        httpEndpoint: 'https://picon.graphql.base-cms.io/graphql',
+        httpLinkOptions: {
+          headers: {
+            'x-tenant-key': 'cygnus_ofcr',
+            'x-cdn-image-hostname': 'base.imgix.net',
+            'x-cdn-asset-hostname': 'media.cygnus.com',
+          },
+        },
+      },
+    },
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
-<template></template>
+<template>
+  <ApolloQuery
+    :query="require('../graphql/websiteScheduledContent')"
+    :variables="{ sectionId: 56199, pagination: { limit: 10 } }"
+  >
+    <template v-slot="{ result: { loading, error, data } }">
+      <!-- Loading -->
+      <div v-if="loading" class="loading apollo">Loading...</div>
+
+      <!-- Error -->
+      <div v-else-if="error" class="error apollo">An error occurred</div>
+
+      <!-- Result -->
+      <div v-else-if="data" class="result apollo">
+        <div v-for="item of data.websiteScheduledContent.edges">
+          {{ item.node.name }}
+        </div>
+      </div>
+
+      <!-- No result -->
+      <div v-else class="no-result apollo">No result :(</div>
+    </template>
+  </ApolloQuery>
+</template>
 
 <script>
 export default {};
 </script>
-
-<style></style>
