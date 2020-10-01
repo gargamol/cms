44a294c39d40b833501e9f5ad19ee70a1f5d2744
Jeremy Laird <Jeremy.Laird@cygnus.com>
2020-10-01 09:17:21 -0500

    Added content redirects

--- pages/redirects/content.vue
+++ pages/redirects/content.vue
@@ -1,22 +1,23 @@
-<template>
-  <h1>Secret page</h1>
-</template>
-
 <script>
 import gql from 'graphql-tag';
 
+// @ToDO replace name with the equivalent of node.siteContext.url
+
 const getSlug = gql`
-query Content($id: Int!) {
-  content(input: { id: $id, status: any }) {
-    slug
-}
+  query Content($id: Int!) {
+    content(input: { id: $id, status: any }) {
+      name
+    }
+  }
 `;
 
 export default {
   mounted() {
-    this.$apollo.query({ query: getSlug, id: Number(this.$route.params.id) }).then(({ data }) => {
-      console.log(('data', data));
-    });
+    this.$apollo
+      .query({ query: getSlug, variables: { id: Number(this.$route.params.id) } })
+      .then(({ data }) => {
+        console.log('name', data.content.name);
+      });
   },
 };
 </script>
